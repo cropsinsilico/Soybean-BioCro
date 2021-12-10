d6eadeac976872186ecb423b9ff541ff890fefbe
leighmatth <53279181+leighmatth@users.noreply.github.com>
2021-12-09 22:12:22 -0600

    update parameter names in data folder files

--- Data/Soybean-BioCro_Parameters/soybean_modules.R
+++ Data/Soybean-BioCro_Parameters/soybean_modules.R
@@ -1,5 +1,5 @@
 # Do the calculations inside an empty list so that temporary variables are not created in .Global.
-soybean_steadystate_modules <- c("soil_type_selector",
+soybean_direct_modules <- c("soil_type_selector",
                                  "stomata_water_stress_linear",
                                  "parameter_calculator",
                                  "soybean_development_rate_calculator",
@@ -14,21 +14,20 @@ soybean_steadystate_modules <- c("soil_type_selector",
                                  "no_leaf_resp_neg_assim_partitioning_growth_calculator",
                                  "senescence_coefficient_logistic")
 
-soybean_derivative_modules <- c("senescence_logistic",
+soybean_differential_modules <- c("senescence_logistic",
                                 "partitioning_growth",
                                 "two_layer_soil_profile",
                                 "development_index",
                                 "thermal_time_linear")
 
 
-soybean_solver_params <- list(
-  type = 'Gro_rkck54',
+soybean_ode_solver <- list(
+  type = 'boost_rkck54',
   output_step_size = 1.0,
   adaptive_rel_error_tol = 1e-4,
   adaptive_abs_error_tol = 1e-4,
   adaptive_max_steps = 200)
-# Note: 'Gro_rsnbrk' and 'Gro' (which defaults to Gro_rsnbrk when an adaptive solver can be
-# used, as in this case) should not be used as the solver type as the model will not simulate
+# Note: 'boost_rsnbrk' should not be used as the solver type as the model will not simulate
 # soybean growth unless the tolerances are stringent (e.g., output_step_size = 0.01,
 # adaptive_rel_error_tol = 1e-9, adaptive_abs_error_tol = 1e-9)
 
